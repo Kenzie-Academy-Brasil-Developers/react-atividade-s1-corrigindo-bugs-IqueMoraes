d9ccc94bf78e09ad45619c0797300bdb7f0fa541
IqueMoraes <iquedemoraes@hotmail.com>
2021-09-17 16:04:31 -0300

    correcoes de bugs

--- src/App.js
+++ src/App.js
@@ -1,43 +1,43 @@
 import CardsList from "./components/cards-list";
-import useEffect from "react";
+import { useState } from "react";
+import { useEffect } from "react";
 import "./App.css";
 
-export const App = () => {
+const App = () => {
   const [showDeck, setShowDeck] = useState(false);
   const [cardsList, setCardsList] = useState([]);
   const [deck, setDeck] = useState("");
 
-  const handleDeckRequest = () => {
+  useEffect(() => {
     fetch("https://deckofcardsapi.com/api/deck/new/")
       .then((res) => res.json())
-      .then((res) => console.log(res));
-  };
-
-  const handleCardsRequest = (deckId) => {
-    fetch("https://deckofcardsapi.com/api/deck/${deckId}/draw/?count=52")
-      .then((res) => res.json())
-      .then((res) => setCardsList([...res.cards]));
-  };
-
-  const handleShowDeck = () => {
-    setShowDeck(false);
-  };
+      .then((res) => setDeck(res.deck_id));
+  }, []);
 
   useEffect(() => {
-    handleDeckRequest();
+    if (deck) {
+      fetch(`https://deckofcardsapi.com/api/deck/${deck}/draw/?count=52`)
+        .then((res) => res.json())
+        .then((res) => setCardsList([...res.cards]));
+    }
   }, [deck]);
 
-  useEffect(() => {
-    if (deck) handleCardsRequest(deck);
-  }, []);
+  // const handleShowDeck = () => {
+  //   setShowDeck(true);
+  // };
 
   return (
     <div className="main-container">
       <h1 className="main-title">Debugue para ver o baralho</h1>
-      <button onChange={handleShowDeck} className="new-deck-button">
+      <button
+        onClick={() => setShowDeck(!showDeck)}
+        className="new-deck-button"
+      >
         Novo baralho
       </button>
-      {ShowDeck & <CardsList cardsList={cardsList} />}
+      {showDeck && <CardsList cardsList={cardsList} />}
     </div>
   );
 };
+
+export default App;
--- src/components/card/index.jsx
+++ src/components/card/index.jsx
@@ -1,7 +1,11 @@
 import "./style.css";
 
-const Card = (card) => {
-  return <img src={card.image} alt={card.code} className="card" />;
+const Card = ({ card, key }) => {
+  return (
+    <div key={key}>
+      <img src={card.image} alt={card.code} className="card" />;
+    </div>
+  );
 };
 
 export default Card;
